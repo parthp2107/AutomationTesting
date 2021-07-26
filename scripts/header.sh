54a5f6bafe9757782048698bb6c64086fe8edb4c
parthp2107 <parthp2107@users.noreply.github.com>
2021-07-26 14:16:15 +0000

    deploy: 02299e5fdaa62534e3018c61c799d4d1d0849c3e

--- scripts/header.sh
+++
@@ -1,9 +0,0 @@
-file=/home/runner/work/AutomationTesting/AutomationTesting/docs/docs/SchemaMarkdown/*.md
-sed -i '1d' $file
-cd /home/runner/work/AutomationTesting/AutomationTesting/docs/docs/SchemaMarkdown/
-for file in *.md
-do
-  sed -i "1i\
-# ${file%%.*}" "$file"
-done
-
