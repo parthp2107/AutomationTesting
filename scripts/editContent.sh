9e98c223ea50291c62ff1913dc5cec5a22eefe6a
parthp2107 <parth.panchal@deuexsoultions.com>
2021-08-03 10:36:18 +0530

    changes

--- scripts/editContent.sh
+++ scripts/editContent.sh
@@ -1,27 +1,24 @@
 echo "----------------------Changing file content----------------------"
 
 path=docs/README.md
-sed -i -e 's#./bots.md#entities/bots.md#g' \
--e 's#./common.md#types/common.md#g' \
--e 's#./dailycount.md#types/dailycount.md#g' \
--e 's#./dashboard.md#entities/dashboard.md#g' \
--e 's#./classification.md#types/classification.md#g' \
--e 's#./database.md#entities/database.md#g' \
--e 's#./databaseservice.md#entities/databaseservice.md#g' \
--e 's#./thread.md#entities/thread.md#g' \
--e 's#./jdbcconnection.md#types/jdbcconnection.md#g' \
--e 's#./metrics.md#entities/metrics.md#g' \
--e 's#./pipeline.md#entities/pipeline.md#g' \
--e 's#./report.md#entities/report.md#g' \
--e 's#./collectiondescriptor.md#types/collectiondescriptor.md#g' \
--e 's#./table.md#entities/table.md#g' \
--e 's#./team.md#entities/team.md#g' \
--e 's#./tagcategory.md#entities/tagcategory.md#g' \
--e 's#./categoryusage.md#types/categoryusage.md#g' \
--e 's#./entityusage.md#types/entityusage.md#g' \
--e 's#./user.md#entities/user.md#g' $path
-
-sed -i '43,306d' $path
+sed -i d $path
+echo "# Schemas
+## Schema Modeling
+OpenMetadata takes the **schema-first** approach to model metadata as a Single Source of Truth with clear vocabulary for the system. First, the Entities and Types in the system are identified and schemas are defined for them. Code is then generated from these schemas and is used in implementing the system. We use [JSON schema](https://json-schema.org/) as the Schema Definition Language as it offers several advantages:
+* Easy to describe the structure and semantics of metadata models with readable documentation that is both human and machine consumable.
+* Common types can be developed once and can be reused as building blocks in other schemas and become the basis of vocabulary development.
+* Models can include structural validation, such as required/optional fields, default values, allowed values, regex that not only serve as automated testing and validation but also as documentation of API contract.
+* A rich set of tools are availble that supports JSON schema support for generating code and validation from JSON schema in various languages, reducing the manual boilerplate coding.
+* Supports rich formats to convert schema types into native standard types during code generation, such as URI, date, and time.
+## Reference
+1. [JSON schema](https://json-schema.org/) specification version [Draft-07 to 2019-099](https://json-schema.org/draft/2019-09/release-notes.html)
+2. [JSON schema 2 POJO](https://www.jsonschema2pojo.org/) tool used for Java code generation from JSON schema
+3. [Data model code generator](https://github.com/koxudaxi/datamodel-code-generator) for generating python code from JSON schema
+## Schemas
+{% page-ref page="\"entities\"" %}
+{% page-ref page="\"types\"" %}
+## Version Note
+The schemas linked above follow the JSON Schema Spec version: `http://json-schema.org/draft-07/schema#`" >> $path
 
 echo "----------------------Changing paths----------------------"
 path=docs/*/*.md
@@ -299,7 +296,6 @@ sed -i -e 's+jdbcconnection-properties-driverclass.md+#jdbcconnection-properties
 -e 's+jdbcconnection-definitions-jdbcinfo-properties-connectionurl.md+#jdbcconnection-definitions-connectionurl+gI' \
 -e 's+jdbcconnection-definitions-connectionurl.md+#jdbcconnection-definitions-connectionurl+gI' \
 -e 's+jdbcconnection-definitions-driverclass.md+#jdbcconnection-definitions-driverclass+gI' $path
-
 echo "----------------------File content changed----------------------"
 
 rm -rf docs/SchemaMarkdown
