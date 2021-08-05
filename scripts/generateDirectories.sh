e17ba2169eabba8877b663977315853b989c8041
parthp2107 <parth.panchal@deuexsoultions.com>
2021-08-05 15:12:12 +0530

    changes

--- scripts/generateDirectories.sh
+++ scripts/generateDirectories.sh
@@ -58,7 +58,7 @@ All relationships are captured using the `EntityReference` type.
 
 Following is an example of a JSON schema of the User entity with attributes id, displayName, and email. User entity has one-to-many relationships to another entity Team \(user is member of multiple teams\).
 
-\```javascript
+```javascript
 {
   "title": "User entity",
   "type": "object",
@@ -85,7 +85,7 @@ Following is an example of a JSON schema of the User entity with attributes id, 
    }
   }
 }
-\```
+```
 ## Metadata system entities
 
 Metadata system has the following core entities:
