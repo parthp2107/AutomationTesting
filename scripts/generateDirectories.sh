429a0754b7ce2eb6ea9896015947f9ab24d223a2
parthp2107 <parth.panchal@deuexsoultions.com>
2021-08-05 15:25:11 +0530

    changes

--- scripts/generateDirectories.sh
+++ scripts/generateDirectories.sh
@@ -123,7 +123,7 @@ else
 fi
 sed -i d $path
 echo "# Types
-JSON schema supports many native types - `null, boolean, object, array, number` and `string`. In addition, to develop clear and consistent vocabulary, domain-specific reusable types are defined ranging from simple types, such as `UUID`, `timestamp`, and `email` to more complex object types, such as `Tags, Ownership` and `Usage`.
+JSON schema supports many native types - null, boolean, object, array, number and string. In addition, to develop clear and consistent vocabulary, domain-specific reusable types are defined ranging from simple types, such as UUID, timestamp, and email to more complex object types, such as Tags, Ownership and Usage.
 ## List of Schema Types
 {% page-ref page="\"basic.md\"" %}
 {% page-ref page="\"collectiondescriptor.md\"" %}
